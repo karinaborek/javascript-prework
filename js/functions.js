e35f708bb4a017e00f94a9dd2dddec648bbe2d19
Karolina <karolina.blaszyk.bd.pl@gmail.com>
2022-04-26 07:52:21 +0200

    Fix functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,16 @@
-function printMessage(msg){
-	let div = document.createElement('div');
+function printMessage(msg) {
+	let div = document.createElement("div");
 	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
-
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
+	document.getElementById("messages").appendChild(div);
+  }
+  
+  function clearMessages() {
+	document.getElementById("messages").innerHTML = "";
+	document.getElementById("result").innerHTML = "";
+  }
+  
+  function printResult(msg) {
+	let div = document.createElement("div");
+	div.innerHTML = msg;
+	document.getElementById("result").appendChild(div);
+  }
